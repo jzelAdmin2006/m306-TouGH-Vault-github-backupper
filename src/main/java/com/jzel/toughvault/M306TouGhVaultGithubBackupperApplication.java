6a36b20872f78628d792744d0db01182f1e2e823
Johannes Zeller <jo.zeller@protonmail.com>
2024-02-27 19:04:00 +0100

    initial commit

---
+++ src/main/java/com/jzel/toughvault/M306TouGhVaultGithubBackupperApplication.java
@@ -0,0 +1,13 @@
+package com.jzel.toughvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M306TouGhVaultGithubBackupperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M306TouGhVaultGithubBackupperApplication.class, args);
+	}
+
+}
