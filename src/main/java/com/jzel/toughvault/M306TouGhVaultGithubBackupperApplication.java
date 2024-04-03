68034d3ffc8efbc419dd9320d03059063fdea4af
Johannes Zeller <jo.zeller@protonmail.com>
2024-04-03 13:37:48 +0200

    improve efficiency

--- src/main/java/com/jzel/toughvault/M306TouGhVaultGithubBackupperApplication.java
+++ src/main/java/com/jzel/toughvault/M306TouGhVaultGithubBackupperApplication.java
@@ -2,10 +2,12 @@ package com.jzel.toughvault;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class M306TouGhVaultGithubBackupperApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/jzel/toughvault/persistence/domain/settings/SettingsPersistence.java
+++ src/main/java/com/jzel/toughvault/persistence/domain/settings/SettingsPersistence.java
@@ -1,7 +1,19 @@
 package com.jzel.toughvault.persistence.domain.settings;
 
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SettingsPersistence extends JpaRepository<SettingsEntity, Integer> {
 
+  @NotNull
+  @Cacheable("settings")
+  List<SettingsEntity> findAll();
+
+  @NotNull
+  @Override
+  @CacheEvict(value = "settings", allEntries = true)
+  <S extends SettingsEntity> S save(@NotNull S settingsEntity);
 }
